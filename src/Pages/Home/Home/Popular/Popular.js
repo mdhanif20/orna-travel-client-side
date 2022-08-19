ad13554bf981d5cf7e47a6d2e2a0e9118e40ff73
mdhainf20 <mdhanif115825@gmail.com>
2022-08-19 12:51:23 +0600

    added all

--- src/Pages/Home/Home/Popular/Popular.js
+++ src/Pages/Home/Home/Popular/Popular.js
@@ -7,10 +7,12 @@ const Popular = (props) => {
     const {name,img,details,rating,time,totalPerson} = customer;
    
     return (
+        
         <div className="card border-0 text-start">
         <img src={img} className="card-img-top w-100" id="popular-Img" alt="..."/>
         <div className="card-body ps-0">
         <h5 className="card-title mb-0">{name}</h5>
+        
         <small className="mb-4">{time} Days Tour </small>
         <p className="card-text">{details.slice(0,90)}....</p>
         <p className="text-bolder fs-5 px-3" id="reting"><span><AiFillStar className="text-warning"/> {rating}</span> <span><BsFillPeopleFill className="text-warning"/> {totalPerson}</span> </p>
--- src/Pages/Home/PopularTour/PopularTour.js
+++ src/Pages/Home/PopularTour/PopularTour.js
@@ -5,7 +5,7 @@ import "./PopularTour.css";
 
 const PopularTour = () => {
     const [customers,setCustomers] = useState([]);
-    axios.get("https://nameless-savannah-22070.herokuapp.com/popular")
+    axios.get("https://blooming-escarpment-12355.herokuapp.com/popular")
     .then(res =>setCustomers(res.data))
     return (
         <div className="px-md-5 px-3 mx-md-4">
